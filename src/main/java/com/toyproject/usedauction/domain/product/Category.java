6719fc4b1dc54febd0f6f27bbd8a454033bd3376
unhappiness404 <unhappiness404@gmail.com>
2023-12-08 16:35:05 +0900

    [#2]feat: Product 클래스 및 연관 클래스 작성

---
+++ src/main/java/com/toyproject/usedauction/domain/product/Category.java
@@ -0,0 +1,25 @@
+package com.toyproject.usedauction.domain.product;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Category {
+
+	@Id
+	@Column(name = "category_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String name;
+
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+}
---
+++ src/main/java/com/toyproject/usedauction/domain/product/OrderPriceType.java
@@ -0,0 +1,10 @@
+package com.toyproject.usedauction.domain.product;
+
+public enum OrderPriceType {
+
+	ONE_HUNDRED,
+	ONE_THOUSAND,
+	FIVE_THOUSAND,
+	TEN_THOUSAND
+
+}
