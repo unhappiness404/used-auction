7b89f838c64bbe11ea5c469f420756858335b326
unhappiness404 <unhappiness404@gmail.com>
2023-12-08 16:35:05 +0900

    [#2]feat: Product 클래스 및 연관 클래스 작성

---
+++ src/main/java/com/toyproject/usedauction/domain/product/Product.java
@@ -0,0 +1,41 @@
+package com.toyproject.usedauction.domain.product;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+	@Id
+	@Column(name = "product_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 20, nullable = false)
+	private String title;
+
+	@Column(length = 500)
+	private String content;
+
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private ClosingTime closingTime;
+
+	@Column(nullable = false)
+	private int startPrice;
+
+	@Column(nullable = false)
+	private int endPrice;
+
+	@Column(nullable = false)
+	private int nowPrice;
+
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private OrderPriceType orderPriceType;
+}
