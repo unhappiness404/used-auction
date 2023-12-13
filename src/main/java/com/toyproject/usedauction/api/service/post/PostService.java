079ea23c9f763eb46fbc8dcf411c32d29494dd7b
unhappiness404 <unhappiness404@gmail.com>
2023-12-13 13:43:08 +0900

    [#2]feat: 게시글 저장 기능 구현

--- src/main/java/com/toyproject/usedauction/api/service/post/PostService.java
+++ src/main/java/com/toyproject/usedauction/api/service/post/PostService.java
@@ -1,25 +1,32 @@
 package com.toyproject.usedauction.api.service.post;
 
 import com.toyproject.usedauction.api.service.post.request.PostCreateRequest;
-import com.toyproject.usedauction.api.service.post.response.ProductResponse;
+import com.toyproject.usedauction.api.service.post.response.PostCreateResponse;
 import com.toyproject.usedauction.domain.category.Category;
 import com.toyproject.usedauction.domain.category.CategoryRepository;
 import com.toyproject.usedauction.domain.post.Post;
 import com.toyproject.usedauction.domain.post.PostRepository;
+import com.toyproject.usedauction.domain.postImage.PostImage;
+import com.toyproject.usedauction.domain.postImage.PostImageRepository;
 import com.toyproject.usedauction.domain.user.User;
 import com.toyproject.usedauction.domain.user.UserRepository;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
 
-	private PostRepository postRepository;
-	private UserRepository userRepository;
-	private CategoryRepository categoryRepository;
+	private final PostRepository postRepository;
+	private final UserRepository userRepository;
+	private final CategoryRepository categoryRepository;
+	private final PostImageRepository postImageRepository;
 
-	public ProductResponse createPost(PostCreateRequest postCreateRequest) {
+	@Transactional
+	public PostCreateResponse createPost(PostCreateRequest postCreateRequest) {
 		User findUser = userRepository.findById(postCreateRequest.getUserId())
 			.orElseThrow(() -> new IllegalArgumentException("해당 user id가 존재하지 않습니다."));
 
@@ -30,7 +37,16 @@ public class PostService {
 
 		Post savePost = postRepository.save(post);
 
-		return ProductResponse.of(savePost);
+		List<String> imageNames = postCreateRequest.getImageNames();
+		List<PostImage> findImages = postImageRepository.findAllByNameIn(imageNames);
+		List<String> imageUrls = new ArrayList<>();
+
+		for (PostImage postImage : findImages) {
+			postImage.setPost(savePost);
+			imageUrls.add(postImage.getUrl());
+		}
+
+		return PostCreateResponse.of(savePost, imageUrls);
 	}
 
 }
