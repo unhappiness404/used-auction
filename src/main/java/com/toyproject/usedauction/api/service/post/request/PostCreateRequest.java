b38868de6825f4cb74db749a5798cbc49d3d8c99
unhappiness404 <unhappiness@404@gmail.com>
2023-12-13 12:39:36 +0900

    [#2]feat: 이미지URL, 경매종료시간 추가

--- src/main/java/com/toyproject/usedauction/api/service/post/request/PostCreateRequest.java
+++ src/main/java/com/toyproject/usedauction/api/service/post/request/PostCreateRequest.java
@@ -1,50 +1,52 @@
 package com.toyproject.usedauction.api.service.post.request;
 
 import com.toyproject.usedauction.domain.category.Category;
-import com.toyproject.usedauction.domain.post.ClosingTime;
+import com.toyproject.usedauction.domain.post.ClosingTimeType;
 import com.toyproject.usedauction.domain.post.OrderPriceType;
 import com.toyproject.usedauction.domain.post.Post;
 import com.toyproject.usedauction.domain.user.User;
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
 @NoArgsConstructor
+@ToString
 public class PostCreateRequest {
 
 	private String title;
 	private String content;
-	private ClosingTime closingTime;
+	private ClosingTimeType closingTimeType;
 	private int startPrice;
 	private int endPrice;
-	private int nowPrice;
 	private OrderPriceType orderPriceType;
 	private Long userId;
 	private Long categoryId;
+	private List<String> imageNames;
 
 	@Builder
-	public PostCreateRequest(String title, String content, ClosingTime closingTime, int startPrice,
-		int endPrice, int nowPrice, OrderPriceType orderPriceType, Long userId, Long categoryId) {
+	private PostCreateRequest(String title, String content, ClosingTimeType closingTimeType, int startPrice,
+		int endPrice, OrderPriceType orderPriceType, Long userId, Long categoryId, List<String> imageNames) {
 		this.title = title;
 		this.content = content;
-		this.closingTime = closingTime;
+		this.closingTimeType = closingTimeType;
 		this.startPrice = startPrice;
 		this.endPrice = endPrice;
-		this.nowPrice = nowPrice;
 		this.orderPriceType = orderPriceType;
 		this.userId = userId;
 		this.categoryId = categoryId;
+		this.imageNames = imageNames;
 	}
 
 	public Post toEntity(User writer, Category category) {
 		return Post.builder()
 			.title(title)
 			.content(content)
-			.closingTime(closingTime)
+			.closingTimeType(closingTimeType)
 			.startPrice(startPrice)
 			.endPrice(endPrice)
-			.nowPrice(nowPrice)
 			.orderPriceType(orderPriceType)
 			.writer(writer)
 			.category(category)
