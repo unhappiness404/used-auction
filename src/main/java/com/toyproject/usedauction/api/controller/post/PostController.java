6b7fd52f47e6032b32b3b32198996e056eb3a977
unhappiness404 <unhappiness404@gmail.com>
2023-12-26 13:53:34 +0900

    [#2]feat: 중복되는 path `@RequestMapping`으로 분리

--- src/main/java/com/toyproject/usedauction/api/controller/post/PostController.java
+++ src/main/java/com/toyproject/usedauction/api/controller/post/PostController.java
@@ -7,16 +7,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class PostController {
 
 	private final PostService postService;
 
-	@PostMapping("/api/posts")
+	@PostMapping("/posts")
 	public PostCreateResponse createPost(@RequestBody PostCreateRequest request) {
 		log.info("request = {}", request);
 		return postService.createPost(request);
