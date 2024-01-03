e0e0867ea32be25a3eb254a4ef912be5689b0fb0
unhappiness404 <unhappiness404@gmail.com>
2024-01-03 14:16:01 +0900

    [#2]feat: controller service 레이어 dto 분리 및 valid 추가

---
+++ src/main/java/com/toyproject/usedauction/api/service/post/request/PostCreateServiceRequest.java
@@ -0,0 +1,50 @@
+package com.toyproject.usedauction.api.service.post.request;
+
+import com.toyproject.usedauction.domain.category.Category;
+import com.toyproject.usedauction.domain.post.ClosingTimeType;
+import com.toyproject.usedauction.domain.post.OrderPriceType;
+import com.toyproject.usedauction.domain.post.Post;
+import com.toyproject.usedauction.domain.user.User;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class PostCreateServiceRequest {
+
+	private String title;
+	private String content;
+	private ClosingTimeType closingTimeType;
+	private int startPrice;
+	private int endPrice;
+	private OrderPriceType orderPriceType;
+	private Long userId;
+	private Long categoryId;
+
+	@Builder
+	private PostCreateServiceRequest(String title, String content, ClosingTimeType closingTimeType, int startPrice,
+		int endPrice, OrderPriceType orderPriceType, Long userId, Long categoryId) {
+		this.title = title;
+		this.content = content;
+		this.closingTimeType = closingTimeType;
+		this.startPrice = startPrice;
+		this.endPrice = endPrice;
+		this.orderPriceType = orderPriceType;
+		this.userId = userId;
+		this.categoryId = categoryId;
+	}
+
+	public Post toEntity(User writer, Category category, LocalDateTime registerDateTime) {
+		return Post.builder()
+			.title(title)
+			.content(content)
+			.closingTimeType(closingTimeType)
+			.registerDateTime(registerDateTime)
+			.startPrice(startPrice)
+			.endPrice(endPrice)
+			.orderPriceType(orderPriceType)
+			.writer(writer)
+			.category(category)
+			.build();
+	}
+}
