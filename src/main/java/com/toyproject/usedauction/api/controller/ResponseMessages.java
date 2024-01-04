dff1a93e4e5252dc794a9e3fced59d980205aacb
unhappiness404 <unhappiness404@gmail.com>
2024-01-04 15:16:49 +0900

    [#2]feat: 게시글 삭제 구현

--- src/main/java/com/toyproject/usedauction/api/controller/ResponseMessages.java
+++ src/main/java/com/toyproject/usedauction/api/controller/ResponseMessages.java
@@ -5,7 +5,11 @@ import lombok.Getter;
 @Getter
 public enum ResponseMessages {
 
-	POST_CREATE_SUCCESS("게시글 작성에 성공했습니다.");
+	POST_CREATE_SUCCESS("게시글 작성에 성공했습니다."),
+
+	POST_UPDATE_SUCCESS("게시글 수정에 성공했습니다."),
+
+	POST_DELETE_SUCCESS("게시글 삭제에 성공했습니다.");
 
 	private final String message;
 
